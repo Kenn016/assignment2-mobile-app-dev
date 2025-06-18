08133546943ad22307f6273bd6bc2468c81fd44d
Kenneth Cellona <kenneth.cellona@edu.sait.ca>
2025-06-17 22:54:30 -0600

    Edited index.tsx file to add styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { Platform, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
 import ToDoForm from '../../components/ToDoForm';
 import ToDoList from '../../components/ToDoList';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <ToDoList />
-      <ToDoForm />
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <ToDoList />
+        <ToDoForm />
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, // Add top padding on Android
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 20,
+  },
+});
